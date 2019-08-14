38dc30ae50147e2da446444992fb959571522bf1
UM2016 <1337706818@qq.com>
2019-08-14 11:10:26 +0800

    线下接口区分

--- js/config.js
+++ js/config.js
@@ -4,13 +4,13 @@
 
     //接口路径
     window.LOCAL = "./remote/";                                     //本地路径
-    window.ROUTE = "http://192.168.1.167/";                         //远程路径
+    window.ROUTE = "http://192.168.1.26/";                          //远程路径
 
     //组件接口
     window.API = {
         //全局接口
         common_tree     : LOCAL+"Common/common_tree.json",          //左侧导航 ROUTE+"User/GetUserPageInfo"
-        common_view     : "./page/welcome.html",                    //欢迎页面 personnel_train_exam
+        common_view     : "./page/personnel_interview.html",        //欢迎页面 personnel_train_exam
 
         //测试接口
         test_datagrid   : LOCAL+"Test/test_datagrid.json",          //数据表格
