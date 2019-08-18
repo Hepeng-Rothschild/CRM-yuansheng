9f06668e6c66abd0b590d9176867aa1da69d46e3
UM2016 <1337706818@qq.com>
2019-08-18 09:56:34 +0800

    remote文件夹整理

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
     $.zui.store.enable=false;
 
     //接口路径
-    window.STATE = "route";                                         //当前状态 local  route
+    window.STATE = "local";                                         //当前状态 local  route
     window.LOCAL = "./remote/";                                     //本地路径
     window.ROUTE = "http://192.168.1.26/";                          //远程路径
     window.REC_PER_PAGE = 20;                                       //每页几条
@@ -11,12 +11,13 @@
     //组件接口
     window.API = {
         //全局接口
-        common_tree     : LOCAL+"Common/common_tree.json",          //左侧导航 ROUTE+"User/GetUserPageInfo"
-        common_view     : "./page/personnel_staff.html",        //欢迎页面 personnel_train_exam
+        common_tree     : LOCAL+"Test/common_tree.json",            //左侧导航 ROUTE+"User/GetUserPageInfo"
+        common_view     : "./page/welcome.html",                    //欢迎页面 welcome personnel_train_exam 
 
         //测试接口
         test_datagrid   : LOCAL+"Test/test_datagrid.json",          //数据表格
         test_response   : LOCAL+"Test/test_response.json",          //数据响应
+        test_export     : LOCAL+"Upload/test_export.pdf",           //导出文件
     }
     /*
     数据表格 宽度标准
