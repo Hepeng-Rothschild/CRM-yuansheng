9af328688cb51b4f9add75185f1bb9621720b207
UM2016 <1337706818@qq.com>
2019-09-09 17:17:47 +0800

    信息发布完成

--- js/config.js
+++ js/config.js
@@ -13,7 +13,7 @@
 
         //本地接口
         window.API = {
-            common_view     : "./page/admin_message_donation.html",                        //welcome admin_message_donation
+            common_view     : "./page/admin_train_case.html",               //welcome
             test_datagrid   : LOCAL+"Test/test_datagrid.json",              //数据表格
             test_response   : LOCAL+"Test/test_response.json",              //数据响应
             test_editor     : LOCAL+"Test/test_editor.php",                 //富文本编辑器
@@ -47,10 +47,6 @@
 
     /*在做页面
     admin_goods             物品管理
-    admin_message_donation  乐捐通知
-    admin_message_meeting   会议纪要
-    admin_message_news      新闻公告
-    admin_train_case        案例管理
     admin_train_center      培训中心
     admin_train_data        资料中心
     admin_train_rule        规章制度
