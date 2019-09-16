d742a6b4de750cdec1783566edbb8a42c47a6e92
UM2016 <1337706818@qq.com>
2019-09-16 10:07:17 +0800

    测试页面新增

--- js/config.js
+++ js/config.js
@@ -1,63 +1,47 @@
+//全局存储关闭
+$.zui.store.enable=false;
 
-    //全局存储关闭
-    $.zui.store.enable=false;
+//接口路径
+window.STATE = "local";                                                 //当前状态 local route
+window.LOCAL = "./remote/";                                             //本地路径
+window.ROUTE = "http://192.168.1.26/";                                  //远程路径
+window.REC_PER_PAGE = 20;                                               //每页几条
 
-    //接口路径
-    window.STATE = "local";                                                 //当前状态 local route
-    window.LOCAL = "./remote/";                                             //本地路径
-    window.ROUTE = "http://192.168.1.26/";                                  //远程路径
-    window.REC_PER_PAGE = 20;                                               //每页几条
+//全局接口
+if( STATE == "local" ){
 
-    //全局接口
-    if( STATE == "local" ){
+    //本地接口
+    window.API = {
+        common_view     : "./page/welcome.html",                        //welcome
+        test_datagrid   : LOCAL+"Test/test_datagrid.json",              //数据表格
+        test_response   : LOCAL+"Test/test_response.json",              //数据响应
+        test_editor     : LOCAL+"Test/test_editor.php",                 //富文本编辑器
+        test_export     : LOCAL+"Upload/test_export.pdf",               //导出文件
 
-        //本地接口
-        window.API = {
-            common_view     : "./page/personnel_contractor.html",           //welcome
-            test_datagrid   : LOCAL+"Test/test_datagrid.json",              //数据表格
-            test_response   : LOCAL+"Test/test_response.json",              //数据响应
-            test_editor     : LOCAL+"Test/test_editor.php",                 //富文本编辑器
-            test_export     : LOCAL+"Upload/test_export.pdf",               //导出文件
-
-            common_tree     : LOCAL+"User/GetUserPageInfo.json",            //左侧导航
-            common_company  : LOCAL+"Company/getCompanyByPositionid.json",  //分公司
-            common_depart   : LOCAL+"Company/getDeparment.json",            //部门
-            common_post     : LOCAL+"Position/getPostionsByDid.json",       //职位
-            common_staff    : LOCAL+"Company/getCompanybyid.json",          //员工
-            IMAGE_DEFAULT   : "./img/image_default.jpg",                    //占位图片
-        }
-
-    } else {
-
-        //远程接口
-        window.API = {
-            common_view     : "./page/welcome.html",                        //welcome
-            test_datagrid   : LOCAL+"Test/test_datagrid.json",              //数据表格
-            test_response   : LOCAL+"Test/test_response.json",              //数据响应
-            test_editor     : LOCAL+"Test/test_editor.php",                 //富文本编辑器
-            test_export     : LOCAL+"Upload/test_export.pdf",               //导出文件
-            
-            common_tree     : LOCAL+"User/GetUserPageInfo.json",            //左侧导航
-            common_company  : ROUTE+"Company/getCompanyByPositionid",       //分公司
-            common_depart   : ROUTE+"Company/getDeparment",                 //部门
-            common_post     : ROUTE+"Position/getPostionsByDid",            //职位
-            common_staff    : ROUTE+"Company/getCompanybyid",               //员工
-            IMAGE_DEFAULT   : "./img/image_default.jpg",                    //占位图片
-        }
-
+        common_tree     : LOCAL+"User/GetUserPageInfo.json",            //左侧导航
+        common_company  : LOCAL+"Company/getCompanyByPositionid.json",  //分公司
+        common_depart   : LOCAL+"Company/getDeparment.json",            //部门
+        common_post     : LOCAL+"Position/getPostionsByDid.json",       //职位
+        common_staff    : LOCAL+"Company/getCompanybyid.json",          //员工
+        IMAGE_DEFAULT   : "./img/image_default.jpg",                    //占位图片
     }
 
-    /*
-    #商务管理
-    信息员列表  business_provider_list
-    信息员管理  business_provider_admin
-    客户跟踪    business_customer_tracked
-    客户管理    business_customer_admin
-    客户审批    business_customer_approval
-    过期审批    business_customer_overdue
-    失败审批    business_customer_failure
-    可抢信息员  business_public_provider_get
-    可抢客户    business_public_customer_get
-    信息员资源  business_public_provider_source
-    客户资源    business_public_customer_source
-    */
+} else {
+
+    //远程接口
+    window.API = {
+        common_view     : "./page/welcome.html",                        //welcome
+        test_datagrid   : LOCAL+"Test/test_datagrid.json",              //数据表格
+        test_response   : LOCAL+"Test/test_response.json",              //数据响应
+        test_editor     : LOCAL+"Test/test_editor.php",                 //富文本编辑器
+        test_export     : LOCAL+"Upload/test_export.pdf",               //导出文件
+        
+        common_tree     : LOCAL+"User/GetUserPageInfo.json",            //左侧导航
+        common_company  : ROUTE+"Company/getCompanyByPositionid",       //分公司
+        common_depart   : ROUTE+"Company/getDeparment",                 //部门
+        common_post     : ROUTE+"Position/getPostionsByDid",            //职位
+        common_staff    : ROUTE+"Company/getCompanybyid",               //员工
+        IMAGE_DEFAULT   : "./img/image_default.jpg",                    //占位图片
+    }
+
+}
