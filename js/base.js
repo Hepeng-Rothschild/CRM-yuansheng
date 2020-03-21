d924da5cd8a7e2a64c5e08ef8152df10bed63671
gituser <1337706818@qq.com>
2020-03-21 18:14:14 +0800

    -

--- js/base.js
+++ js/base.js
@@ -323,6 +323,7 @@ function um_date(fmt,date){
         "S": now.getMilliseconds()                 //毫秒
     };
     if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (now.getFullYear() + "").substr(4 - RegExp.$1.length));
+    
     for (var k in o)
         if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
     return fmt;
